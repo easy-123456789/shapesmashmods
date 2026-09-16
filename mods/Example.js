c2cbd0cb3ad1d1905c995d9af0f4b134228c561c
easy-123456789 <bobbybobby852025@hotmail.com>
2026-09-16 15:58:02 -0400

    Add the example mod to /mods

---
+++ mods/Example.js
@@ -0,0 +1,15 @@
+Mods.registerCharacter({
+  key: 'green',
+  name: 'Green Fighter',
+  color: '#22cc66',
+  attackMultiplier: 1.2,
+  speed: 1.3,
+  keys: {
+    left: 'a',
+    right: 'd',
+    jump: 'w',
+    attack: 'f',
+    final: 'g',
+    special: 's'
+  }
+});
