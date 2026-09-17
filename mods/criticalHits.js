a2699d345bca158e7dfbd523556ea9235553cb5b
easy-123456789 <bobbybobby852025@hotmail.com>
2026-09-17 17:17:17 -0400

    Add topstreak tracking for critical hits

--- mods/criticalHits.js
+++ mods/criticalHits.js
@@ -1,8 +1,16 @@
 Mods.registerGameplayMod({
   onHit({ attacker, defender, damage, source }) {
+    // Make sure char has a topstreak
+    if(!attacker.topstreak) attacker.topstreak = 0;
     if(Math.random() <= 0.01){
       defender.percent -= (10 * attacker.attackMultiplier);
       addCommentary(`${attacker.name} Lands a crit!`);
+    }
+    if(attacker.vy > 0){
+      attacker.topstreak += 1;
+      defender.percent -= (5 * (attacker.topstreak + 1));
+      addCommentary(`${attacker.name} Lands a down crit!`)
+      
     }
   }
 })
