84210f26e12d5f9326c4fd680fec86b23c323604
easy-123456789 <bobbybobby852025@hotmail.com>
2026-09-17 15:15:18 -0400

    Increase random gravity effect by 0.5

--- mods/randomGravity.js
+++ mods/randomGravity.js
@@ -1,7 +1,7 @@
 Mods.registerGameplayMod({
   onFrame({ p1, p2, gameOver }) {
     if (gameOver || !p1 || !p2) return;
-    grav = Math.random() + 0.5;
+    grav = Math.random() + 1;
     // Random-gravity effect.
     p1.vy *= grav;
     p2.vy *= grav;
