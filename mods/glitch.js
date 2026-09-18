a97c6f987741ed151811b60ef22fb6d29d5b2ecf
easy-123456789 <bobbybobby852025@hotmail.com>
2026-09-18 15:22:08 -0400

    Add Glitch H@cker character with special ability

--- mods/glitch.js
+++ mods/glitch.js
@@ -1,1 +1,18 @@
-
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+Mods.registerCharacter({
+  key: 'glitch',
+  name: 'Glitch H@cker',
+  color: '#22dd99',
+  upSpecial(fighter, opponent){
+    if(Math.random() < 0.5){
+      for(let i = 0; i < getRandomIntInclusive(1.10); i++){
+        fighter.specialCooldown = 0;
+        fighter.special(opponent);
+      }
+    } else {
+      opponent.percent += 20;
+    }
+  }
+})
