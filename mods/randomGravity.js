ea8935be80235f95c059c68a93d29f2ffcea0200
easy-123456789 <bobbybobby852025@hotmail.com>
2026-09-20 14:47:03 -0400

    Change gravity effect to random vertical velocity

--- mods/randomGravity.js
+++ mods/randomGravity.js
@@ -3,7 +3,7 @@ Mods.registerGameplayMod({
     if (gameOver || !p1 || !p2) return;
     grav = Math.random() + 1;
     // Random-gravity effect.
-    p1.vy *= grav;
-    p2.vy *= grav;
+    p1.vy = Math.random() * 20 - 10;
+    p2.vy = Math.random() * 20 - 10;
   }
 })
