3e3e15ca5198061101888a55eebab13a02a38c2d
easy-123456789 <bobbybobby852025@hotmail.com>
2026-09-20 14:15:08 -0400

    Refactor upSpecial and downSpecial methods

--- mods/glitch.js
+++ mods/glitch.js
@@ -1,13 +1,17 @@
 function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 Mods.registerCharacter({
   key: 'glitch',
   name: 'Glitch H@cker',
   color: '#22dd99',
-  upSpecial(fighter, opponent){
-    if(Math.random() < 0.5){
-      for(let i = 0; i < getRandomIntInclusive(1,10); i++){
+
+  upSpecial(fighter, opponent) {
+    if (Math.random() < 0.5) {
+      const repeats = getRandomIntInclusive(1, 10);
+
+      for (let i = 0; i < repeats; i++) {
         fighter.specialCooldown = 0;
         fighter.special(opponent);
       }
@@ -15,12 +19,16 @@ Mods.registerCharacter({
       opponent.percent += 20;
     }
   },
-  downSpecial(fighter, opponent){
-    const args = [fighter, opponent];
-    let r = getRandomIntInclusive(0, 3);
+
+  downSpecial(fighter, opponent) {
+    const r = getRandomIntInclusive(0, 3);
+    const args = [opponent, fighter];
+
+    // Directional specials use (fighter, direction, opponent).
     if (r === 3) {
-      args.splice(1, 0, fighter.facing
+      args.splice(1, 0, opponent.facing > 0 ? 'right' : 'left');
     }
+
     opponent.specials[r](...args);
   }
-})
+});
