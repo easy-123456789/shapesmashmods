f9a619221402da03e46eabd23b5aeea759b5a649
easy-123456789 <bobbybobby852025@hotmail.com>
2026-09-22 15:27:32 -0400

    Enhance second chance feature with commentary
    Add commentary when defender gets a second chance.

--- mods/secondChances/secondChance.js
+++ mods/secondChances/secondChance.js
@@ -1,7 +1,10 @@
 Mods.registerGameplayMod({
   onHit({attacker, defender, source}){
     if(defender.percent >= (attacker.percent * 3)){
-      setTimeout(() => {defender.vy = 0}, Math.random() * 1000)
+      setTimeout(() => {
+        defender.vy = 0;
+        addCommentary(defender.name + "Gets a second chance!")
+      }, Math.random() * 1000)
     }
   }
 })
